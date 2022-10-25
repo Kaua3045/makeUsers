0f0c654ae4cdcd14a3476830330dc122d75f5d74
Kauã Pereira <zkauacodes@hotmail.com>
2022-10-25 18:05:27 -0300

    refactor: refactor tables create and create connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,17 +1,15 @@
 const { Pool } = require('pg')
 
+const client = new Pool({
+  host: process.env.HOST_PG,
+  port: process.env.PORT_PG,
+  user: process.env.USER_PG,
+  password: process.env.PASSWORD_PG,
+  database: process.env.DATABASE_PG
+})
+
 const createConnection = async () => {
-  const client = new Pool({
-    host: process.env.HOST_PG,
-    port: process.env.PORT_PG,
-    user: process.env.USER_PG,
-    password: process.env.PASSWORD_PG,
-    database: process.env.DATABASE_PG
-  })
-
   await client.connect()
-
-  return { client }
 }
 
-module.exports = { createConnection }
+module.exports = { createConnection, client }
--- src/database/tables.js
+++ src/database/tables.js
@@ -1,9 +1,9 @@
-const { createConnection } = require('./connection')
+const { createConnection, client } = require('./connection')
 
 const createTables = async () => {
-  const { client } =  await createConnection()
+  await createConnection()
 
-  await client.query('CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT, email TEXT UNIQUE, password TEXT)')
+  await client.query('CREATE TABLE IF NOT EXISTS users (id UUID PRIMARY KEY, name TEXT, email TEXT UNIQUE, password TEXT)')
 }
 
 module.exports = createTables
