408a7930308c2c88b5b81f678e945c58497202f5
Kauã Pereira <zkauacodes@hotmail.com>
2022-10-25 20:38:22 -0300

    add: adding uuid generation in model constructor

--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,16 @@
+const { v4: uuid } = require('uuid')
+
 class User {
+  id
   name
   email
   password
 
   constructor(name, email, password) {
+    if (!this.id) {
+      this.id = uuid()
+    }
+
     this.name = name
     this.email = email
     this.password = password
