f6765b6d00aaee4dc07d478514d5a6e4b6963561
Kauã Pereira <zkauacodes@hotmail.com>
2022-11-06 10:00:50 -0300

    add: adding route to auth and configure other routes to request token auth

--- src/routes.js
+++ src/routes.js
@@ -9,10 +9,15 @@ const {
   resetPasswordUserController 
 } = require('./controller/userController')
 
-routes.get('/', getAllUsersController)
+const { loginController } = require('./controller/authController')
+const { auth } = require('./middlewares/authMiddleware')
+
+routes.get('/', auth, getAllUsersController)
 routes.post('/create', createUserController)
-routes.put('/update/:id', updateUserController)
+routes.put('/update/:id', auth, updateUserController)
 routes.put('/resetpassword/:id', resetPasswordUserController)
-routes.delete('/delete/:id', deleteUserController)
+routes.delete('/delete/:id', auth, deleteUserController)
+
+routes.post('/auth', loginController)
 
 module.exports = routes
