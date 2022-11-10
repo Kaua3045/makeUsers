95f99a8bbcca8a57864a967eef0a2fd05d471f26
Kauã Pereira <zkauacodes@hotmail.com>
2022-11-10 18:38:01 -0300

    add: adding avatar_url in sql query

--- src/services/users/getAllUsersService.js
+++ src/services/users/getAllUsersService.js
@@ -2,7 +2,7 @@ const { client } = require('../../database/connection')
 
 module.exports = {
   async getAllUsers() {
-    const { rows } = await client.query('SELECT id, name, email FROM users');
+    const { rows } = await client.query('SELECT id, name, email, avatar_url FROM users');
     const users = rows
 
     return users
--- src/services/users/getUserByIdService.js
+++ src/services/users/getUserByIdService.js
@@ -3,7 +3,7 @@ const AppError = require('../../errors/appError')
 
 module.exports = {
   async getUserById(id) {
-    const { rows } = await client.query('SELECT id, name, email FROM users WHERE id = $1', [ id ])
+    const { rows } = await client.query('SELECT id, name, email, avatar_url FROM users WHERE id = $1', [ id ])
     const user = rows[0]
 
     if (!user) {
