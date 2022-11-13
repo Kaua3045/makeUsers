fbb049b9a67bc6c3e694a02e0509598697682746
Kauã Pereira <zkauacodes@hotmail.com>
2022-11-13 14:31:58 -0300

    add: adding a getProductImageById method

---
+++ src/services/products/getProductImageByIdService.js
@@ -0,0 +1,15 @@
+const { client } = require('../../database/connection')
+const AppError = require('../../errors/appError')
+
+module.exports = {
+  async getProductImageById(id) {
+    const imageDatabase = await client.query('SELECT * FROM products_images WHERE id = $1', [id])
+    const imageExists = imageDatabase.rows[0]
+
+    if (!imageExists) {
+      throw new AppError('Image does not exists!')
+    }
+
+    return imageExists
+  }
+}
