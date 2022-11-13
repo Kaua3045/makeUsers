c760bb4521d1224d8bb5dc785fa1192071cc4363
Kauã Pereira <zkauacodes@hotmail.com>
2022-11-13 14:34:32 -0300

    add: adding deleteProductImage, getProductImageById and getAllProductAndImages methods in controller

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -4,6 +4,9 @@ const { createProduct } = require("../services/products/createProductService")
 const { createProductImages } = require('../services/products/createProductImageService')
 const { getProductById } = require("../services/products/getProductByIdService")
 const { deleteProduct } = require("../services/products/deleteProductService")
+const { deleteProductImage } = require("../services/products/deleteProductImageService")
+const { getProductImageById } = require("../services/products/getProductImageByIdService")
+const { getAllProductAndImages } = require("../services/products/getAllProductService")
 
 module.exports = {
   async createProductController(req, res) {
@@ -34,9 +37,28 @@ module.exports = {
     return res.json(result)
   },
 
+  async getProductImageByIdController(req, res) {
+    const { id } = req.params
+
+    const result = await getProductImageById(id)
+    return res.json(result)
+  },
+
+  async getAllProductController(req, res) {
+    const result = await getAllProductAndImages()
+    return res.json(result)
+  },
+
   async deleteProductController(req, res) {
     const { id } = req.params
     await deleteProduct(id)
+
+    return res.status(204).end()
+  },
+
+  async deleteProductImageController(req, res) {
+    const { id } = req.params
+    await deleteProductImage(id)
 
     return res.status(204).end()
   }
